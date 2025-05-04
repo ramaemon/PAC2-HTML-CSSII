2bd3961ae9396bde50df5a511db342e08e315b84
rmaestrem <rmaestrem@uoc.edu>
2025-05-04 13:39:09 +0200

    He començat el projecte de nou. Estic intentant fer funcionar la càrrega de de HTML dinàmicament. He creat home, article i afegit codi js en el main. A banda d'installar Bootstrap

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,36 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+import * as bootstrap from 'bootstrap'
+
 /**
  * Write any other JavaScript below
  */
+window.addEventListener('load', () => {
+  const container = document.querySelector('.views-container');
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+  if(!container) {
+    console.error("Esto es un error");
+    return;
+  }
+
+   const loadView = async (view) => {
+     try {
+      const response = await fetch(`/views/${view}.html`);
+      console.log(response);
+      
+      if(!response.ok)
+        throw new Error('No se puede cargar la vista.');
+      
+      const html = await response.text();
+      container.innerHTML = html;
+     } catch (err) {
+      container.innerHTML = '<p>Error</p>';
+      console.error(err);
+
+     }
+   }
+
+   loadView('home');
+
+});
