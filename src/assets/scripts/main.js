93d23eea69e7da7f0315f7d958a2e3deb2ab8d53
rmaestrem <rmaestrem@uoc.edu>
2025-05-05 16:49:05 +0200

    Corregit error navegació per segona volta

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,7 +6,7 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
-import * as bootstrap from 'bootstrap'
+// import * as bootstrap from 'bootstrap'
 
 /**
  * Write any other JavaScript below
